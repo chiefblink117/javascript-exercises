82f63ca98a5d21fbf31f0cc30bc4ed3416d42dd6
Diego <maciasg.diego17@gmail.com>
2024-02-18 17:51:06 -0600

    forth exercise complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+// let array = [1, 2, 3, 4];
+const removeFromArray = function(arr, ...values) {
+    return arr.filter(element => !values.includes(element));
 };
 
 // Do not edit below this line
