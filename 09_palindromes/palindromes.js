dd65a26bef4cec5361ed159088728caf39ec5ad6
Diego <maciasg.diego17@gmail.com>
2024-06-03 22:17:47 -0500

    09_palindromes completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let arr = string.replace(/[^a-zA-Z]/g, '').toLowerCase().split(' ').join('');
+    let arrReverse = [...arr].reverse();
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] !== arrReverse[i]) {
+            return false;
+        }
+    } return true;
 };
 
 // Do not edit below this line
