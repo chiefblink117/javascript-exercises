764cf97f01c29fd98b3dd67328a9701972ed006b
Diego <maciasg.diego17@gmail.com>
2024-06-07 13:41:23 -0500

    10_fibonacci exercise completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num >= 0) {
+        let numbers = [0,1];
+        for (i=0; i < num; i++){
+            let sum = numbers[i] + numbers[i+1];
+            numbers.push(sum);
+        }
+        return numbers[num];
+    } else return ("OOPS");
 };
 
 // Do not edit below this line
