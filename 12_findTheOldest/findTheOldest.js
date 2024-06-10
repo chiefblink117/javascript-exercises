fc46894feb0e4e96f22f2e111ac1dbbe471b21ba
Diego <maciasg.diego17@gmail.com>
2024-06-10 14:47:56 -0500

    12_findTheOldest exercise completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const oldest = arr.reduce((oldestPerson, currentPerson) => {
+        let lifespan = 0;
+        if (currentPerson.yearOfDeath){
+            lifespan = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+        } else {
+            const currentDate = new Date();
+            lifespan = currentDate.getFullYear() - currentPerson.yearOfBirth;
+        }
+        if (lifespan > oldestPerson.lifespan) {
+            return {...currentPerson, lifespan};
+          }
+        return oldestPerson;
+    }, {lifespan: 0});
+    return oldest;
 };
 
 // Do not edit below this line
